6e4bfb437918005df34e374ae7ced9caa539c157
Jethro Nederhof <jethro@snowflake-analytics.com>
2018-04-10 12:09:31 +1000

    Move utility methods to util module

--- src/Beacon.ts
+++ src/Beacon.ts
@@ -58,68 +58,9 @@ function parseBeacon(beacon) {
     return result;
 }
 
-const hasMembers = (obj) => {
-    if (typeof obj !== 'object' || obj === null) {
-        return false;
-    }
-
-    if (Array.isArray(obj) && obj.length > 0) {
-        return true;
-    }
-
-    for (const p in obj) {
-        if (obj.hasOwnProperty(p)) {
-            return true;
-        }
-    }
-
-    return false;
-};
-
-const nameType = (val) => {
-    if (val === null) {
-        return 'null';
-    }
-    if (Array.isArray(val)) {
-        return 'array';
-    }
-    if (typeof val === 'number' && isNaN(val)) {
-        return 'number (NaN)';
-    }
-    if (typeof val === 'number' && !isFinite(val)) {
-        return 'number (Infinite)';
-    }
-    if (val instanceof RegExp) {
-        return 'RegExp';
-    }
-    if (val instanceof Date) {
-        return 'Date';
-    }
-    if (val instanceof Promise) {
-        return 'Promise';
-    }
-    return typeof val;
-};
-
-const copyToClipboard = (text) => {
-    let cb = document.getElementById('clipboard') as HTMLInputElement;
-    if (cb === null) {
-        cb = document.createElement('input') as HTMLInputElement;
-        cb.type = 'text';
-        cb.id = 'clipboard';
-        cb.style.position = 'relative';
-        cb.style.left = '-10000px';
-        document.body.appendChild(cb);
-    }
-
-    cb.value = typeof text === 'string' ? text : JSON.stringify(text);
-    cb.select();
-    document.execCommand('copy');
-};
-
 const labelType = (val) => m('button.typeinfo.button.is-pulled-right.is-info',
-        {onclick: () => copyToClipboard(val), title: 'Click to copy'},
-        nameType(val))
+        {onclick: () => util.copyToClipboard(val), title: 'Click to copy'},
+        util.nameType(val))
 ;
 
 const contextToTable = (obj) => {
@@ -141,8 +82,8 @@ const contextToTable = (obj) => {
         } else {
             for (p in obj.data) {
                 if (obj.data.hasOwnProperty(p)) {
-                    const type = nameType(obj.data[p]);
-                    if ((type === 'object' || type === 'array') && hasMembers(obj.data[p])) {
+                    const type = util.nameType(obj.data[p]);
+                    if ((type === 'object' || type === 'array') && util.hasMembers(obj.data[p])) {
                         rows.push(m('tr', [
                             m('th', p),
                             m('td', contextToTable(obj.data[p])),
@@ -173,7 +114,7 @@ const contextToTable = (obj) => {
                     if (validity === 'Unrecognised') {
                         chrome.runtime.openOptionsPage();
                     } else {
-                        copyToClipboard(errorText);
+                        util.copyToClipboard(errorText);
                     }
                 }, title: errorText}, validity),
                 m('textarea.card-footer-item[disabled]', {value: JSON.stringify(obj)}),
@@ -182,8 +123,8 @@ const contextToTable = (obj) => {
     } else {
         for (p in obj) {
             if (obj.hasOwnProperty(p)) {
-                const type = nameType(obj[p]);
-                if ((type === 'object' || type === 'array') && hasMembers(obj[p])) {
+                const type = util.nameType(obj[p]);
+                if ((type === 'object' || type === 'array') && util.hasMembers(obj[p])) {
                     rows.push(m('tr', [
                         m('th', p),
                         m('td', contextToTable(obj[p])),
