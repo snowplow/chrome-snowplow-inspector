7d5b6dd3ce167f0670f4a6842742758013a2171b
Jethro Nederhof <jethro@poplindata.com>
2019-07-02 09:33:17 +1000

    Add DevTools theme name detection

--- src/extension.ts
+++ src/extension.ts
@@ -4,4 +4,11 @@ import SnowplowInspector = require('./components/SnowplowInspector');
 // tslint:disable-next-line
 require('./style/inspector.scss');
 
+// tslint:disable-next-line
+declare module chrome.devtools.panels {
+    export const themeName: string;
+}
+
+document.body.classList.add(chrome.devtools.panels.themeName);
+
 m.mount(document.body, SnowplowInspector);
