9e58da4f385230f60be55a8a83be9561bb844513
Jethro Nederhof <jethro@jethron.id.au>
2021-04-30 15:02:21 +1000

    Remove no longer required type correction

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,6 @@
 import m = require('mithril');
 import SnowplowInspector = require('./components/SnowplowInspector');
 
-// tslint:disable-next-line
-declare module chrome.devtools.panels {
-    export const themeName: string;
-}
-
 document.body.classList.add('theme' + chrome.devtools.panels.themeName);
 
 m.mount(document.body, SnowplowInspector);
