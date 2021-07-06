e1ab2a5db281fdec70b64639de249da245db1dcf
Jethro Nederhof <jethro@jethron.id.au>
2021-07-06 12:06:38 +1000

    Fix imports

---
+++ src/components/Modals/index.ts
@@ -0,0 +1,4 @@
+import { BadRowsModal } from "./BadRowsModal";
+import { LiveStreamModal } from "./LiveStreamModal";
+
+export { BadRowsModal, LiveStreamModal };
---
+++ src/ts/iglu/index.ts
@@ -0,0 +1,4 @@
+import { Resolver } from "./Resolver";
+import { IgluSchema } from "./IgluSchema";
+
+export { IgluSchema, Resolver };
