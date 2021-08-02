77c76a0f5904d731e990efd162b810396d3aa64f
Jethro Nederhof <jethro@jethron.id.au>
2021-08-02 17:36:55 +1000

    Initial static registry manifest support

--- src/ts/iglu/Registries/Static.ts
+++ src/ts/iglu/Registries/Static.ts
@@ -9,12 +9,12 @@ const REQUEST_TIMEOUT_MS = 5000;
 export class StaticRegistry extends Registry {
   private cache: Map<IgluSchema, ResolvedIgluSchema> = new Map();
   private readonly base: URL;
-  private readonly manifest: string;
+  private readonly manifest?: URL;
 
   constructor(spec: RegistrySpec) {
     super(spec);
     this.base = new URL(spec["uri"]);
-    this.manifest = spec["manifestUri"];
+    this.manifest = spec["manifestUri"] && new URL(spec["manifestUri"]);
   }
 
   private fetch(schemaPath: string): ReturnType<typeof fetch> {
@@ -56,19 +56,62 @@ export class StaticRegistry extends Registry {
     return Promise.resolve(this.lastStatus || "OK");
   }
 
-  walk() {
-    const schemas: IgluSchema[] = Array.from(this.cache.values());
+  parseManifest(): Promise<IgluSchema[]> {
+    const fileListProps = ["tree", "files", "paths"];
     if (this.manifest) {
-      return this.fetch(this.manifest)
+      return this.fetch(this.manifest.href)
         .then((res) => res.json())
-        .then((man) => (Array.isArray(man) ? man.map(IgluSchema.fromUri) : []))
-        .then((claimed) => {
-          const seen = new Set<string>();
+        .then((man) => {
+          let list: unknown[] = [];
 
-          return schemas
-            .concat(claimed.filter((s): s is IgluSchema => s !== null))
-            .filter((s) => !seen.has(s.uri()) && seen.add(s.uri()));
+          if (typeof man === "object" && man) {
+            if (Array.isArray(man)) {
+              list = man;
+            } else {
+              for (const p of fileListProps) {
+                if (man.hasOwnProperty(p) && Array.isArray(man[p])) {
+                  list.push(...man[p]);
+                }
+              }
+            }
+
+            return list
+              .filter(
+                (val: unknown): val is string | { path: string } =>
+                  typeof val === "string" ||
+                  (typeof val === "object" &&
+                    !!val &&
+                    typeof (val as any)["path"] === "string")
+              )
+              .map((almostSchema): IgluUri | null => {
+                const maybeUri =
+                  typeof almostSchema === "string"
+                    ? almostSchema
+                    : almostSchema.path;
+                const schemaparts =
+                  /(([^\/+])\/([^\/+])\/jsonschema\/([\d+]-[\d+]-[\d+]))(\.json(schema)?)?$/.exec(
+                    maybeUri
+                  );
+                return schemaparts && (("iglu:" + schemaparts[1]) as IgluUri);
+              })
+              .map((uri) => uri && IgluSchema.fromUri(uri))
+              .filter((schema): schema is IgluSchema => schema !== null);
+          } else return [];
         });
-    } else return Promise.resolve(schemas);
+    } else return Promise.resolve([]);
+  }
+
+  walk() {
+    return this.parseManifest().then((claimed) => {
+      const claimedUris = claimed.map(String);
+      const cachedSchemas: IgluSchema[] = [];
+      for (const cached of this.cache.values()) {
+        if (!claimedUris.includes(cached.toString())) {
+          cachedSchemas.push(cached);
+        }
+      }
+
+      return claimed.concat(cachedSchemas);
+    });
   }
 }
