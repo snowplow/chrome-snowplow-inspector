5bde31f543fad262a8aaf4130d8477dedbdb0518
Jethro Nederhof <jethro@jethron.id.au>
2021-08-11 23:19:27 +1000

    iglu: reorganize imports

--- src/ts/iglu/index.ts
+++ src/ts/iglu/index.ts
@@ -1,5 +1,33 @@
-import { Registry } from "./Registries";
+import {
+  Registry,
+  RegistrySpec,
+  DataStructuresRegistry,
+  IgluRegistry,
+  LocalRegistry,
+  StaticRegistry,
+} from "./Registries";
 import { Resolver } from "./Resolver";
 import { IgluSchema, IgluUri, ResolvedIgluSchema } from "./IgluSchema";
 
-export { IgluSchema, IgluUri, Registry, ResolvedIgluSchema, Resolver };
+const build = (spec: RegistrySpec): Registry => {
+  switch (spec.kind) {
+    case "ds":
+      return new DataStructuresRegistry(spec);
+    case "iglu":
+      return new IgluRegistry(spec);
+    case "local":
+      return new LocalRegistry(spec);
+    case "static":
+      return new StaticRegistry(spec);
+  }
+  throw new Error(`Unsupported Registry: ${spec}`);
+};
+
+export {
+  build as buildRegistry,
+  IgluSchema,
+  IgluUri,
+  Registry,
+  ResolvedIgluSchema,
+  Resolver,
+};
