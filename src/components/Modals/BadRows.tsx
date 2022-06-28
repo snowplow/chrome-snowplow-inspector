ee2be9f303632097bab3e1ad717c163d3c229d66
Jethro Nederhof <jethron@poplindata.com>
2022-06-28 12:59:43 +1000

    BadRows: support reading files in addition to paste

--- src/components/Modals/BadRows.tsx
+++ src/components/Modals/BadRows.tsx
@@ -1,6 +1,6 @@
 import { Entry } from "har-format";
 import { h, FunctionComponent } from "preact";
-import { useState } from "preact/hooks";
+import { useCallback, useState } from "preact/hooks";
 
 import { badToRequests } from "../../ts/util";
 import { ModalOptions } from ".";
@@ -14,6 +14,28 @@ export const BadRows: FunctionComponent<BadRowsOptions> = ({
   setModal,
 }) => {
   const [badRows, setBadRows] = useState<string[]>([]);
+
+  const onImport = useCallback((e: ClipboardEvent | DragEvent) => {
+    e.preventDefault();
+    const transfer = "clipboardData" in e ? e.clipboardData : e.dataTransfer;
+
+    if (transfer !== null) {
+      if (transfer.files.length) {
+        Promise.all(
+          Array.from(transfer.files).map((file) =>
+            file.text().then((text) => text.split("\n"))
+          )
+        ).then((inputs) =>
+          setBadRows((badRows) => badRows.concat(inputs.flat()))
+        );
+      } else {
+        setBadRows((badRows) =>
+          badRows.concat(transfer.getData("text").trim().split("\n"))
+        );
+      }
+    }
+  }, []);
+
   return (
     <div class="modal is-active">
       <div class="modal-background" onClick={() => setModal()}></div>
@@ -25,26 +47,19 @@ export const BadRows: FunctionComponent<BadRowsOptions> = ({
         <section class="modal-card-body">
           <p>
             Bad Rows occur when events fail to validate during enrichment. You
-            can paste your bad data straight from S3 (1 JSON object per line),
-            or, for real-time data, the JSON object from the _source field in
-            ElasticSearch or just the line property of that object. Keep pasting
-            to include events in bulk. Invalid payloads (OPTIONS requests, no
-            data, invalid JSON, bots, etc.) will be ignored.
+            can paste your bad data straight from S3 (1 JSON object per line, or
+            an entire file with that format). For real-time data, the JSON
+            object from the <code>_source</code> field in ElasticSearch or just
+            the <code>line</code> property of that object. Keep pasting to
+            include events in bulk. Invalid payloads (OPTIONS requests, no data,
+            invalid JSON, bots, etc.) will be ignored.
           </p>
           <textarea
             class="textarea"
             placeholder="Paste JSONL or base 64 events here, one per line"
             rows={1}
-            onPaste={(e: ClipboardEvent) => {
-              e.preventDefault();
-              setBadRows((badRows) => {
-                if (e.clipboardData !== null) {
-                  return badRows.concat(
-                    e.clipboardData.getData("text").trim().split("\n")
-                  );
-                } else return badRows;
-              });
-            }}
+            onDrop={onImport}
+            onPaste={onImport}
           />
           <p>Number of events to the to import: {badRows.length}</p>
         </section>
