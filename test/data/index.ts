167999edd7cdf629e3d76391206826cc73e9a86f
Jethro Nederhof <jethro.nederhof@snowplowanalytics.com>
2025-12-02 14:34:57 +1100

    Format test code/data

--- test/data/index.ts
+++ test/data/index.ts
@@ -1,16 +1,16 @@
 import type { Har } from "har-format";
 
-import {default as microGet } from "./micro-get.har.json" with { type: "json" };
-import {default as microPost } from "./micro-post.har.json" with { type: "json" };
-import {default as microCustom } from "./micro-postpath.har.json" with { type: "json" };
-import {default as badPayloadViolation } from "./collector_payload_format_violation.json" with { type: "json" };
+import { default as microGet } from "./micro-get.har.json" with { type: "json" };
+import { default as microPost } from "./micro-post.har.json" with { type: "json" };
+import { default as microCustom } from "./micro-postpath.har.json" with { type: "json" };
+import { default as badPayloadViolation } from "./collector_payload_format_violation.json" with { type: "json" };
 
 export const micro: Record<string, Har> = {
-	get: microGet,
-	post: microPost,
-	custom: microCustom,
+  get: microGet,
+  post: microPost,
+  custom: microCustom,
 };
 
 export const bad = {
-	collector_payload_format_violation: badPayloadViolation,
+  collector_payload_format_violation: badPayloadViolation,
 } as const;
